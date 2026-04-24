ea658f283689e10f727b2318ed8e837b5b729c9e
Reyner <reynerelopez@gmail.com>
2026-04-24 01:08:09 -0600

    sp de insertar empleado

---
+++ SQL/05_sp_InsertarEmpleado.sql
@@ -0,0 +1,100 @@
+﻿CREATE PROCEDURE dbo.sp_InsertarEmpleado
+    @inIdPuesto INT,
+    @inDocumento VARCHAR(20),
+    @inNombre VARCHAR(100),
+    @inFechaContratacion DATE,
+    @inIdPostByUser INT,
+    @inPostInIP VARCHAR(50),
+    @outResultCode INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+
+        -- validacion de que el documento no este repedito
+        IF EXISTS (
+            SELECT 1
+            FROM dbo.Empleado
+            WHERE ValorDocumentoIdentidad = @inDocumento
+        )
+        BEGIN
+            SET @outResultCode = 50002;
+            RETURN;
+        END
+
+        -- validacion de que el nombre no este repetido
+        IF EXISTS (
+            SELECT 1
+            FROM dbo.Empleado
+            WHERE Nombre = @inNombre
+        )
+        BEGIN
+            SET @outResultCode = 50003;
+            RETURN;
+        END
+
+        -- para insertar empleado
+        INSERT INTO dbo.Empleado (
+            IdPuesto,
+            ValorDocumentoIdentidad,
+            Nombre,
+            FechaContratacion,
+            SaldoVacaciones,
+            EsActivo,
+            IdPostByUser,
+            PostInIP
+        )
+        VALUES (
+            @inIdPuesto,
+            @inDocumento,
+            @inNombre,
+            @inFechaContratacion,
+            0,
+            1,
+            @inIdPostByUser,
+            @inPostInIP
+        );
+        
+        --bitacora
+        INSERT INTO dbo.BitacoraEvento (
+            idTipoEvento,
+            Descripcion,
+            IdPostByUser,
+            PostInIP
+        )
+        VALUES (
+            3,
+            'Empleado insertado correctamente',
+            @inIdPostByUser,
+            @inPostInIP
+        );
+
+        SET @outResultCode = 0;
+
+    END TRY
+    BEGIN CATCH
+
+        SET @outResultCode = 50008;
+
+        INSERT INTO dbo.DBError (
+            UserName,
+            Number,
+            State,
+            Severity,
+            Line,
+            [Procedure],
+            Message
+        )
+        VALUES (
+            'sp_InsertarEmpleado',
+            ERROR_NUMBER(),
+            ERROR_STATE(),
+            ERROR_SEVERITY(),
+            ERROR_LINE(),
+            ERROR_PROCEDURE(),
+            ERROR_MESSAGE()
+        );
+
+    END CATCH
+END;
