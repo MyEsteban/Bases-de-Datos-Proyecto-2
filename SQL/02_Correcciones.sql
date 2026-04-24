82569ff833853a49b6bf429480c9a43faa1ccc59
Reyner <reynerelopez@gmail.com>
2026-04-23 23:59:26 -0600

    añadi algunas correcciones y un sp de login

---
+++ SQL/02_Correcciones.sql
@@ -0,0 +1,21 @@
+﻿ALTER TABLE dbo.Empleado
+ADD CONSTRAINT UQ_Empleado_Documento UNIQUE (ValorDocumentoIdentidad);
+
+ALTER TABLE dbo.Empleado
+ADD CONSTRAINT UQ_Empleado_Nombre UNIQUE (Nombre);
+
+ALTER TABLE dbo.Error
+ADD CONSTRAINT UQ_Error_Codigo UNIQUE (Codigo);
+
+ALTER TABLE dbo.Empleado
+ADD IdPostByUser INT NULL,
+    PostInIP VARCHAR(50) NULL,
+    PostTime DATETIME DEFAULT GETDATE();
+
+EXEC sp_rename 'dbo.Movimiento.IdUsuario', 'IdPostByUser', 'COLUMN';
+
+EXEC sp_rename 'dbo.Movimiento.IpPostIn', 'PostInIP', 'COLUMN';
+
+EXEC sp_rename 'dbo.BitacoraEvento.IdUsuario', 'IdPostByUser', 'COLUMN';
+
+EXEC sp_rename 'dbo.BitacoraEvento.IpPostIn', 'PostInIP', 'COLUMN';
---
+++ SQL/03_sp_Login.sql
@@ -0,0 +1,81 @@
+﻿CREATE PROCEDURE dbo.sp_Login
+    @inUsername VARCHAR(50),
+    @inPassword VARCHAR(50),
+    @inIP VARCHAR(50),
+    @outResultCode INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @userId INT;
+
+    BEGIN TRY
+
+        SELECT @userId = Id
+        FROM dbo.Usuario
+        WHERE Username = @inUsername
+          AND Password = @inPassword;
+
+        -- Login fallido
+        IF @userId IS NULL
+        BEGIN
+            SET @outResultCode = 50001;
+
+            INSERT INTO dbo.BitacoraEvento (
+                idTipoEvento,
+                Descripcion,
+                IdPostByUser,
+                PostInIP
+            )
+            VALUES (
+                2,
+                'Login fallido',
+                NULL,
+                @inIP
+            );
+
+            RETURN;
+        END
+
+        -- Login exitoso
+        SET @outResultCode = 0;
+
+        INSERT INTO dbo.BitacoraEvento (
+            idTipoEvento,
+            Descripcion,
+            IdPostByUser,
+            PostInIP
+        )
+        VALUES (
+            1,
+            'Login exitoso',
+            @userId,
+            @inIP
+        );
+
+    END TRY
+    BEGIN CATCH
+
+        SET @outResultCode = 50008;
+
+        INSERT INTO dbo.DBError (
+            UserName,
+            Number,
+            State,
+            Severity,
+            Line,
+            [Procedure],
+            Message
+        )
+        VALUES (
+            @inUsername,
+            ERROR_NUMBER(),
+            ERROR_STATE(),
+            ERROR_SEVERITY(),
+            ERROR_LINE(),
+            ERROR_PROCEDURE(),
+            ERROR_MESSAGE()
+        );
+
+    END CATCH
+END;
