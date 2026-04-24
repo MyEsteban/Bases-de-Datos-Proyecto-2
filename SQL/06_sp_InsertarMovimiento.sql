c4a1c02ea12773f73167eefeae022d15827c3389
Reyner <reynerelopez@gmail.com>
2026-04-24 10:12:50 -0600

    añadi sp de insertar movimiento y corregi el sp de login

---
+++ SQL/06_sp_InsertarMovimiento.sql
@@ -0,0 +1,112 @@
+﻿CREATE PROCEDURE dbo.sp_InsertarMovimiento
+    @inIdEmpleado INT,
+    @inIdTipoMovimiento INT,
+    @inMonto DECIMAL(10,2),
+    @inFecha DATE,
+    @inIdPostByUser INT,
+    @inPostInIP VARCHAR(50),
+    @outResultCode INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE
+        @saldoActual DECIMAL(10,2),
+        @nuevoSaldo DECIMAL(10,2),
+        @tipoAccion VARCHAR(10);
+
+    BEGIN TRY
+
+        SELECT
+            @saldoActual = SaldoVacaciones
+        FROM dbo.Empleado
+        WHERE Id = @inIdEmpleado;
+
+        SELECT
+            @tipoAccion = TipoAccion
+        FROM dbo.TipoMovimiento
+        WHERE Id = @inIdTipoMovimiento;
+
+        -- Crédito o Débito
+        IF @tipoAccion = 'Credito'
+            SET @nuevoSaldo = @saldoActual + @inMonto;
+        ELSE
+            SET @nuevoSaldo = @saldoActual - @inMonto;
+
+        -- Validar saldo negativo
+        IF @nuevoSaldo < 0
+        BEGIN
+            SET @outResultCode = 50010;
+            RETURN;
+        END
+
+        BEGIN TRANSACTION
+
+            INSERT INTO dbo.Movimiento (
+                IdEmpleado,
+                IdTipoMovimiento,
+                Fecha,
+                Monto,
+                NuevoSaldo,
+                IdPostByUser,
+                PostInIP
+            )
+            VALUES (
+                @inIdEmpleado,
+                @inIdTipoMovimiento,
+                @inFecha,
+                @inMonto,
+                @nuevoSaldo,
+                @inIdPostByUser,
+                @inPostInIP
+            );
+
+            UPDATE dbo.Empleado
+            SET SaldoVacaciones = @nuevoSaldo
+            WHERE Id = @inIdEmpleado;
+
+            INSERT INTO dbo.BitacoraEvento (
+                idTipoEvento,
+                Descripcion,
+                IdPostByUser,
+                PostInIP
+            )
+            VALUES (
+                4,
+                'Movimiento insertado correctamente',
+                @inIdPostByUser,
+                @inPostInIP
+            );
+
+        COMMIT TRANSACTION
+
+        SET @outResultCode = 0;
+
+    END TRY
+    BEGIN CATCH
+
+        ROLLBACK TRANSACTION;
+
+        SET @outResultCode = 50008;
+
+        INSERT INTO dbo.DBError (
+            UserName,
+            Number,
+            State,
+            Severity,
+            Line,
+            [Procedure],
+            Message
+        )
+        VALUES (
+            'sp_InsertarMovimiento',
+            ERROR_NUMBER(),
+            ERROR_STATE(),
+            ERROR_SEVERITY(),
+            ERROR_LINE(),
+            ERROR_PROCEDURE(),
+            ERROR_MESSAGE()
+        );
+
+    END CATCH
+END;
--- SQL/07_sp_Login_Corregido.sql
+++ SQL/07_sp_Login_Corregido.sql
@@ -1,6 +1,4 @@
-﻿-- Bro cristian, no uses este sp porque le faltaban algunas validaciones. Usa el 07 que es el que ya arreglé.
-
-CREATE PROCEDURE dbo.sp_Login
+﻿CREATE PROCEDURE dbo.sp_Login
     @inUsername VARCHAR(50),
     @inPassword VARCHAR(50),
     @inIP VARCHAR(50),
@@ -9,16 +7,48 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    DECLARE @userId INT;
+    DECLARE
+        @userId INT,
+        @intentosFallidos INT;
 
     BEGIN TRY
+--aqui cuenta la cantidad de intentos fallidos en los ultimos 20 minutos
+        SELECT
+            @intentosFallidos = COUNT(*)
+        FROM dbo.BitacoraEvento
+        WHERE idTipoEvento = 2
+          AND PostInIP = @inIP
+          AND PostTime >= DATEADD(MINUTE, -20, GETDATE());
 
-        SELECT @userId = Id
+   --si se intenta muchas cveces se tiene que bloquear
+        IF @intentosFallidos >= 5
+        BEGIN
+            SET @outResultCode = 50004;
+
+            INSERT INTO dbo.BitacoraEvento (
+                idTipoEvento,
+                Descripcion,
+                IdPostByUser,
+                PostInIP
+            )
+            VALUES (
+                2,
+                'Login deshabilitado',
+                NULL,
+                @inIP
+            );
+
+            RETURN;
+        END
+
+        --busca si el usuario es valido
+        SELECT
+            @userId = Id
         FROM dbo.Usuario
         WHERE Username = @inUsername
           AND Password = @inPassword;
 
-        -- Login fallido
+        --y si no existe el login falla
         IF @userId IS NULL
         BEGIN
             SET @outResultCode = 50001;
@@ -31,7 +61,7 @@ BEGIN
             )
             VALUES (
                 2,
-                'Login fallido',
+                'Intento fallido #' + CAST(@intentosFallidos + 1 AS VARCHAR),
                 NULL,
                 @inIP
             );
@@ -39,7 +69,7 @@ BEGIN
             RETURN;
         END
 
-        -- Login exitoso
+        -- Login funciona
         SET @outResultCode = 0;
 
         INSERT INTO dbo.BitacoraEvento (
@@ -50,7 +80,7 @@ BEGIN
         )
         VALUES (
             1,
-            'Login exitoso',
+            'Exitoso',
             @userId,
             @inIP
         );
