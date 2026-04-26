9a9e1da709b36917ce4b9463a0a8c0ad533e1560
Crisbor77 <crisborm12@gmail.com>
2026-04-26 13:12:09 -0600

    se crea sp que "borra" empleado

---
+++ SQL/21_spBorrarEmpleado.sql
@@ -0,0 +1,76 @@
+CREATE OR ALTER PROCEDURE dbo.sp_BorrarEmpleado
+    @inId INT
+    , @inIdUsuario INT -- Usuario que ejecuta el borrado
+    , @inIpPostIn VARCHAR(64)
+    , @outResultCode INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY
+        SET @outResultCode = 0;
+
+        -- 1. CONTROL DE FLUJO: Verificar si el empleado existe y está activo
+        IF (NOT EXISTS(SELECT 1 FROM dbo.Empleado AS E WHERE (E.Id = @inId) AND (E.EsActivo = 1)))
+        BEGIN
+            SET @outResultCode = 50002; -- El empleado no existe o ya está borrado
+            RETURN;
+        END
+
+        -- 2. TRANSACCIÓN: Borrado lógico
+        BEGIN TRANSACTION;
+
+            UPDATE E
+            SET E.EsActivo = 0
+                , E.PostTime = GETDATE()
+                , E.IpPostIn = @inIpPostIn
+            FROM dbo.Empleado AS E
+            WHERE (E.Id = @inId);
+
+            -- Registro en Bitácora (R7)
+            INSERT INTO dbo.BitacoraEvento (
+                IdTipoEvento
+                , Descripcion
+                , IdUsuario
+                , IpPostIn
+                , PostTime
+            )
+            VALUES (
+                9 -- IdTipoEvento para 'Borrado lógico exitoso'
+                , CONCAT('Borrado lógico de empleado ID: ', CAST(@inId AS VARCHAR))
+                , @inIdUsuario
+                , @inIpPostIn
+                , GETDATE()
+            );
+
+        COMMIT TRANSACTION;
+
+    END TRY
+    BEGIN CATCH
+        IF (@@TRANCOUNT > 0) 
+            ROLLBACK TRANSACTION;
+
+        SET @outResultCode = 50008; 
+        
+        INSERT INTO dbo.DBError (
+            UserName
+            , Number
+            , State
+            , Severity
+            , Line
+            , [Procedure]
+            , Message
+            , DateTime
+        )
+        VALUES (
+            SUSER_SNAME()
+            , ERROR_NUMBER()
+            , ERROR_STATE()
+            , ERROR_SEVERITY()
+            , ERROR_LINE()
+            , 'sp_BorrarEmpleado'
+            , ERROR_MESSAGE()
+            , GETDATE()
+        );
+    END CATCH
+END;
+GO
