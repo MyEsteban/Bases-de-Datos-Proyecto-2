cceb520e0248398504f0e44a7347e61ed2da6cb3
Crisbor77 <crisborm12@gmail.com>
2026-04-26 12:19:36 -0600

    se corrige sp_login

--- SQL/03_sp_Login.sql
+++ SQL/03_sp_Login.sql
@@ -1,83 +1,120 @@
-﻿-- Bro cristian, no uses este sp porque le faltaban algunas validaciones. Usa el 07 que es el que ya arreglé.
-
-CREATE PROCEDURE dbo.sp_Login
-    @inUsername VARCHAR(50),
-    @inPassword VARCHAR(50),
-    @inIP VARCHAR(50),
-    @outResultCode INT OUTPUT
+CREATE OR ALTER PROCEDURE dbo.sp_Login
+    @inUsername VARCHAR(100)
+    , @inPassword VARCHAR(100)
+    , @inIpPostIn VARCHAR(64)
+    , @outResultCode INT OUTPUT
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    DECLARE @userId INT;
+    DECLARE
+        @userId INT
+        , @intentosFallidos INT;
 
     BEGIN TRY
+        SET @outResultCode = 0;
 
-        SELECT @userId = Id
-        FROM dbo.Usuario
-        WHERE Username = @inUsername
-          AND Password = @inPassword;
+        -- Contar intentos fallidos en últimos 20 minutos por IP
+        SELECT
+            @intentosFallidos = COUNT(*)
+        FROM dbo.BitacoraEvento AS BE
+        WHERE (BE.IdTipoEvento = 2) -- Paréntesis obligatorios en condiciones
+          AND (BE.IpPostIn = @inIpPostIn)
+          AND (BE.PostTime >= DATEADD(MINUTE, -20, GETDATE()));
 
-        -- Login fallido
-        IF @userId IS NULL
+        -- Bloqueo por exceso de intentos (R1)
+        IF (@intentosFallidos >= 5)
+        BEGIN
+            SET @outResultCode = 50003; -- Código según catálogo de errores
+
+            INSERT INTO dbo.BitacoraEvento (
+                IdTipoEvento
+                , Descripcion
+                , IdUsuario
+                , IpPostIn
+                , PostTime
+            )
+            VALUES (
+                3 -- Evento de bloqueo
+                , 'Login deshabilitado'
+                , NULL
+                , @inIpPostIn
+                , GETDATE()
+            );
+
+            RETURN;
+        END
+
+        -- Búsqueda de usuario con alias obligatorio
+        SELECT
+            @userId = U.Id
+        FROM dbo.Usuario AS U
+        WHERE (U.Username = @inUsername)
+          AND (U.Password = @inPassword);
+
+        -- Manejo de login fallido
+        IF (@userId IS NULL)
         BEGIN
             SET @outResultCode = 50001;
 
             INSERT INTO dbo.BitacoraEvento (
-                idTipoEvento,
-                Descripcion,
-                IdPostByUser,
-                PostInIP
+                IdTipoEvento
+                , Descripcion
+                , IdUsuario
+                , IpPostIn
+                , PostTime
             )
             VALUES (
-                2,
-                'Login fallido',
-                NULL,
-                @inIP
+                2
+                , 'Intento fallido #' + CAST(@intentosFallidos + 1 AS VARCHAR)
+                , NULL
+                , @inIpPostIn
+                , GETDATE()
             );
 
             RETURN;
         END
 
         -- Login exitoso
-        SET @outResultCode = 0;
-
         INSERT INTO dbo.BitacoraEvento (
-            idTipoEvento,
-            Descripcion,
-            IdPostByUser,
-            PostInIP
+            IdTipoEvento
+            , Descripcion
+            , IdUsuario
+            , IpPostIn
+            , PostTime
         )
         VALUES (
-            1,
-            'Login exitoso',
-            @userId,
-            @inIP
+            1
+            , 'Exitoso'
+            , @userId
+            , @inIpPostIn
+            , GETDATE()
         );
 
     END TRY
     BEGIN CATCH
-
         SET @outResultCode = 50008;
 
         INSERT INTO dbo.DBError (
-            UserName,
-            Number,
-            State,
-            Severity,
-            Line,
-            [Procedure],
-            Message
+            UserName
+            , Number
+            , State
+            , Severity
+            , Line
+            , [Procedure]
+            , Message
+            , DateTime
         )
         VALUES (
-            @inUsername,
-            ERROR_NUMBER(),
-            ERROR_STATE(),
-            ERROR_SEVERITY(),
-            ERROR_LINE(),
-            ERROR_PROCEDURE(),
-            ERROR_MESSAGE()
+            @inUsername
+            , ERROR_NUMBER()
+            , ERROR_STATE()
+            , ERROR_SEVERITY()
+            , ERROR_LINE()
+            , ERROR_PROCEDURE()
+            , ERROR_MESSAGE()
+            , GETDATE()
         );
-
     END CATCH
 END;
+GO
