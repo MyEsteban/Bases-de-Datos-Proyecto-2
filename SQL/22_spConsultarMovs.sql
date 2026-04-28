b371fc040dff78573632fae5ca32694d7da31a22
Crisbor77 <crisborm12@gmail.com>
2026-04-28 09:10:31 -0600

    Refactor sp_ConsultarMovimientos for clarity and structure

--- SQL/22_spConsultarMovs.sql
+++ SQL/22_spConsultarMovs.sql
@@ -1,6 +1,12 @@
-CREATE OR ALTER PROCEDURE dbo.sp_ConsultarMovimientos
+/****** Object:  StoredProcedure [dbo].[sp_ConsultarMovimientos]    Script Date: 28/4/2026 09:09:56 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[sp_ConsultarMovimientos]
     @inIdEmpleado INT
-    , @inIdUsuario INT -- Usuario que consulta para la bitácora
+    , @inIdUsuario INT 
     , @inIpPostIn VARCHAR(64)
     , @outResultCode INT OUTPUT
 AS
@@ -9,14 +15,14 @@ BEGIN
     BEGIN TRY
         SET @outResultCode = 0;
 
-        -- 1. CONTROL DE FLUJO: Verificar que el empleado exista
-        IF (NOT EXISTS(SELECT 1 FROM dbo.Empleado AS E WHERE (E.Id = @inIdEmpleado)))
+        -- 1. CONTROL DE FLUJO: Verificar existencia del empleado usando la tabla física
+        IF (NOT EXISTS(SELECT 1 FROM dbo.Empleado WHERE Id = @inIdEmpleado))
         BEGIN
             SET @outResultCode = 50002; -- Empleado no existe
             RETURN;
         END
 
-        -- 2. REGISTRO EN BITÁCORA (R7)
+        -- 2. REGISTRO EN BITÁCORA: Usando las columnas exactas de tu tabla
         INSERT INTO dbo.BitacoraEvento (
             IdTipoEvento
             , Descripcion
@@ -25,32 +31,34 @@ BEGIN
             , PostTime
         )
         VALUES (
-            15 -- Asumiendo Id para 'Consulta de movimientos'
+            15 -- ID para 'Consulta de movimientos'
             , CONCAT('Consulta de movimientos para empleado ID: ', CAST(@inIdEmpleado AS VARCHAR))
             , @inIdUsuario
             , @inIpPostIn
             , GETDATE()
         );
 
-        -- 3. CONSULTA DE MOVIMIENTOS (R5)
-        -- Traemos el detalle vinculando con TipoMovimiento para saber si fue Crédito o Débito
+        -- 3. CONSULTA FINAL: Alineada con el modelo Movimiento.cs y las tablas físicas
         SELECT 
             M.Id
             , M.Fecha
             , TM.Nombre AS TipoMovimiento
-            , TM.TipoAccion -- Para saber si sumó o restó
+            , TM.TipoAccion 
             , M.Monto
             , M.NuevoSaldo
+            , U.Username AS NombreUsuario -- Alias para mapear a la propiedad en C#
+            , M.PostTime            -- Columna física de la tabla Movimiento
         FROM dbo.Movimiento AS M
-        INNER JOIN dbo.TipoMovimiento AS TM 
-            ON (M.IdTipoMovimiento = TM.Id)
+        INNER JOIN dbo.TipoMovimiento AS TM ON (M.IdTipoMovimiento = TM.Id)
+        INNER JOIN dbo.Usuario AS U ON (M.IdUsuario = U.Id)
         WHERE (M.IdEmpleado = @inIdEmpleado)
-        ORDER BY M.Fecha DESC, M.Id DESC; -- Los más recientes primero
+        ORDER BY M.Fecha DESC, M.Id DESC;
 
     END TRY
     BEGIN CATCH
         SET @outResultCode = 50008; 
         
+        -- Registro de error en tabla administrativa
         INSERT INTO dbo.DBError (
             UserName
             , Number
@@ -73,4 +81,3 @@ BEGIN
         );
     END CATCH
 END;
-GO
